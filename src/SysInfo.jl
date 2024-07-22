c9390dc76e784a015a4ec1119d03a540d9cf54a0
Carsten Bauer <crstnbr@gmail.com>
2024-07-22 08:23:10 +0200

    precompile

--- src/SysInfo.jl
+++ src/SysInfo.jl
@@ -5,4 +5,15 @@ include("implementation.jl")
 
 export ncputhreads, ncores, nsockets, nnuma, hyperthreading_is_enabled, sysinfo
 
+import .Implementation: clear_cache
+
+# precompile
+import PrecompileTools
+PrecompileTools.@compile_workload begin
+    redirect_stdout(Base.DevNull()) do
+        sysinfo()
+    end
+    clear_cache()
+end
+
 end
