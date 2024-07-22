81a2b93f32bc1ab2907606386ecf7beee70f54ec
Carsten Bauer <crstnbr@gmail.com>
2024-07-22 10:39:08 +0200

    minor

--- src/SysInfo.jl
+++ src/SysInfo.jl
@@ -4,8 +4,6 @@ include("utils.jl")
 include("api.jl")
 include("internals.jl")
 
-import .Internals: clear_cache, getsystem, ngpus, check_consistency_backends
-
 # public API
 export sysinfo
 @public ncputhreads, ncores, nsockets, nnuma, hyperthreading_is_enabled
@@ -16,7 +14,7 @@ PrecompileTools.@compile_workload begin
     redirect_stdout(Base.DevNull()) do
         sysinfo()
     end
-    clear_cache()
+    SysInfo.Internals.clear_cache()
 end
 
 end
